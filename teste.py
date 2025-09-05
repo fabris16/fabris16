f0b36048ec0ed3ff506d06ebab99ce20b94d4d16
stefannysilva <fabrisdasilvagustavo@gmail.com>
2025-09-05 07:58:44 -0300

    estudando pyton

--- teste.py
+++ teste.py
@@ -3,3 +3,9 @@ print(5+7)
 print(3.14159)
 Nome="gustavo fabris"
 print("meu nome é",Nome)
+Pi=3.14
+print(type(Pi))
+Pontuação_vôlei=500
+print(type(Pontuação_vôlei))
+Data="15/10/2025"
+print(type(Data))
